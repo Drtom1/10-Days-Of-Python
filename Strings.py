1bac5f69ad407b2f84636aa38eff9396cf71f5b6
Drtom1 <tomogbonna11@gmail.com>
2025-02-03 19:12:03 +0100

    first commit

---
+++ Strings.py
@@ -0,0 +1,10 @@
+#most of this will just be on formatting strings in python
+first_name = 'Thomas'
+last_name = 'Ogbonna'
+#output = 'Hello, ' + first_name + last_name
+#print(output)
+#output = 'Hello, {} {}'.format(first_name, last_name)
+output = f'Hello, {first_name} {last_name}'
+print(output)
+#this is pretty much my favourite way of string formatting in python
+
