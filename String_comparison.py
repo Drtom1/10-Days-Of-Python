6e59d132e8bf1e3a3e416b427279ab26abb67d38
Drtom1 <tomogbonna11@gmail.com>
2025-02-05 18:16:33 +0100

    comparing strings

---
+++ String_comparison.py
@@ -0,0 +1,14 @@
+favourite_food = input('What is your favourite meal?: ')
+if favourite_food == 'fried rice':
+    print('So you must be a good chef!')
+else:
+    print('I was thinking you liked fried rice')
+
+# in python when comparing two strings, they are not equal if they are not in the same case
+
+
+if favourite_food.lower() == 'fried rice':
+    print('So you must be a good chef! ')
+
+# this will compare lower case version of input
+# and gives an output based on conditions specifiedsS
