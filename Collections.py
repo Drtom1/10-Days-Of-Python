63f24e60f2a4e48fb7284b9dc7ad018a72175a27
Drtom1 <tomogbonna11@gmail.com>
2025-02-06 19:44:49 +0100

    collections in python

---
+++ Collections.py
@@ -0,0 +1,15 @@
+thomas = {}
+thomas['first'] = 'Thomas'
+thomas['last'] = 'Ogbonna'
+
+jennifer = {}
+jennifer['first'] = 'Jennifer'
+jennifer['last'] = 'Banks'
+
+people =[]
+people.append(thomas)
+people.append(jennifer)
+people.append({
+    'first': 'Elon', 'last': 'Musk'
+})
+print (people)
